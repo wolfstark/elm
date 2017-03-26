81a6ebc9e081c7a01cd2e52c8c64ab9c928ae439
daywatch <daywatch@192.168.0.101>
2017-03-26 21:24:38 +0800

    update

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,9 +1,191 @@
 <template>
-  <div>i am goods</div>
-</template>
-<!--<script type="text/ecmascript-6">-->
-<script>
-</script>
-<style lang="scss">
+    <div class="goods">
+        <div class="menu-wrapper">
+            <ul class="item-wrapper">
+                <li v-for="item in goods"
+                    class="menu-item">
+                    <span class="text border-1px">
+                            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+                    </span>
+                </li>
+            </ul>
+        </div>
+        <div class="foods-wrapper">
+            <ul>
+                <li v-for="item in goods"
+                    class="good-list">
+                    <h1 class="title">{{item.name}}</h1>
+                    <ul>
+                        <li v-for="food in item.foods" class="food-item border-1px">
+                            <div class="icon">
+                                <img width="57" height="57" :src="food.icon">
+                            </div>
+                            <div class="content">
+                                <h2 class="name">{{food.name}}</h2>
+                                <p class="desc">{{food.description}}</p>
+                                <div class="extra">
+                                    <span class="count">月售{{food.sellCount}}份</span>
+                                    <span>好评率{{food.rating}}%</span>
+                                </div>
+                                <div class="price">
+                                    <span class="now">¥{{food.price}}</span>
+                                    <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                                </div>
 
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<style lang="scss">
+@import "../../common/scss/_mixin.scss";
+.goods {
+    position: absolute;
+    width: 100%;
+    top: 174px;
+    bottom: 46px;
+    overflow: hidden;
+    display: flex;
+    .menu-wrapper {
+        flex: 0 0 80px;
+        width: 80px;
+        background: #f3f5f7;
+        .item-wrapper {
+            margin: 0;
+            padding: 0;
+            .menu-item {
+                display: table;
+                height: 54px;
+                width: 56px;
+                line-height: 14px;
+                padding: 0 12px;
+                .icon {
+                    display: inline-block;
+                    width: 12px;
+                    height: 12px;
+                    margin-right: 2px;
+                    background-size: 12px 12px;
+                    background-repeat: no-repeat;
+                    vertical-align: top;
+                    &.decrease {
+                        @include bg-img('decrease_3');
+                    }
+                    &.discount {
+                        @include bg-img('discount_3');
+                    }
+                    &.guarantee {
+                        @include bg-img('guarantee_3');
+                    }
+                    &.invoice {
+                        @include bg-img('invoice_3');
+                    }
+                    &.special {
+                        @include bg-img('special_3');
+                    }
+                }
+                .text {
+                    display: table-cell;
+                    vertical-align: middle;
+                    width: 56px;
+                    font-size: 12px;
+                    @include border-1px(rgba(7, 17, 27, 0.1), bottom, after);
+                }
+            }
+        }
+    }
+    .foods-wrapper {
+        flex: 1;
+        .title {
+            padding-left: 14px;
+            height: 26px;
+            line-height: 26px;
+            border-left: 2px solid #d9dde1;
+            font-size: 12px;
+            color: rgb(147, 153, 159);
+            background: #f3f5f7;
+        }
+        .food-item {
+            display: flex;
+            margin: 18px;
+            padding-bottom: 18px;
+            @include border-1px(rgba(7, 17, 27, 0.1), bottom, after);
+            &:last-child{
+                @include border-none;
+                margin-bottom: 0;
+            }
+            .icon{
+                flex: 0 0 57px;
+                margin-right: 10px;
+            }
+            .content{
+                flex: 1;
+                .name{
+                    margin: 2px 0 8px 0;
+                    height: 14px;
+                    line-height: 14px;
+                    font-size: 14px;
+                    color: rgb(7, 17, 27);
+                }
+                .desc, .extra{
+                    line-height: 10px;
+                    font-size: 10px;
+                    color: rgb(147, 153, 159);
+                }
+                .desc{
+                    margin-bottom: 8px;
+                }
+                .extra{
+                    line-height: 10px;
+                    .count{
+                        margin-right: 12px;
+                    }
+                }
+                .price{
+                    font-weight: 700;
+                    line-height: 24px;
+                    .now{
+                        margin-right: 8px;
+                        font-size: 14px;
+                        color: rgb(240,20,20);
+                    }
+                    .old{
+                        text-decoration: line-through;
+                        font-size: 10px;
+                        color: rgb(147, 153, 159);
+                    }
+                }
+            }
+        }
+    }
+}
 </style>
+<script>
+
+const ERR_OK = 0;
+export default {
+    props: {
+        seller: {
+            type: Object,
+        },
+    },
+    data() {
+        return {
+            goods: [],
+        };
+    },
+    created() {
+        this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+
+        this.$http.get('/api/goods').then((response) => {
+            const result = response.body;
+            if (ERR_OK === 0) {
+                this.goods = result.data;
+            }
+        });
+    },
+};
+
+</script>
