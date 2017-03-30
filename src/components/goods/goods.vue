893d87556b0891b128cf147c3af80272a0dddb08
daywatch <daywatch@192.168.0.101>
2017-03-30 23:02:39 +0800

    update

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,24 +1,30 @@
 <template>
     <div class="goods">
-        <div class="menu-wrapper">
+        <div class="menu-wrapper"
+             ref="menuWrapper">
             <ul class="item-wrapper">
-                <li v-for="item in goods"
+                <li v-for="(item,index) in goods"
+                    :class="{current:currentIndex===index}"
                     class="menu-item">
                     <span class="text border-1px">
-                            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+                                                                                                                                                                                        <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
                     </span>
                 </li>
             </ul>
         </div>
-        <div class="foods-wrapper">
+        <div class="foods-wrapper"
+             ref="foodsWrapper">
             <ul>
                 <li v-for="item in goods"
-                    class="good-list">
+                    class="food-list food-list-hook">
                     <h1 class="title">{{item.name}}</h1>
                     <ul>
-                        <li v-for="food in item.foods" class="food-item border-1px">
+                        <li v-for="food in item.foods"
+                            class="food-item border-1px">
                             <div class="icon">
-                                <img width="57" height="57" :src="food.icon">
+                                <img width="57"
+                                     height="57"
+                                     :src="food.icon">
                             </div>
                             <div class="content">
                                 <h2 class="name">{{food.name}}</h2>
@@ -29,7 +35,8 @@
                                 </div>
                                 <div class="price">
                                     <span class="now">¥{{food.price}}</span>
-                                    <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                                    <span class="old"
+                                          v-show="food.oldPrice">¥{{food.oldPrice}}</span>
                                 </div>
 
                             </div>
@@ -62,6 +69,16 @@
                 width: 56px;
                 line-height: 14px;
                 padding: 0 12px;
+                &.current {
+                    position: relative;
+                    z-index: 10;
+                    margin-top: -1px;
+                    background: #fff;
+                    font-weight: 700;
+                    .text {
+                        @include border-none;
+                    }
+                }
                 .icon {
                     display: inline-block;
                     width: 12px;
@@ -112,46 +129,48 @@
             margin: 18px;
             padding-bottom: 18px;
             @include border-1px(rgba(7, 17, 27, 0.1), bottom, after);
-            &:last-child{
+            &:last-child {
                 @include border-none;
                 margin-bottom: 0;
             }
-            .icon{
+            .icon {
                 flex: 0 0 57px;
                 margin-right: 10px;
             }
-            .content{
+            .content {
                 flex: 1;
-                .name{
+                .name {
                     margin: 2px 0 8px 0;
                     height: 14px;
                     line-height: 14px;
                     font-size: 14px;
                     color: rgb(7, 17, 27);
                 }
-                .desc, .extra{
+                .desc,
+                .extra {
                     line-height: 10px;
                     font-size: 10px;
                     color: rgb(147, 153, 159);
                 }
-                .desc{
+                .desc {
                     margin-bottom: 8px;
+                    line-height: 12px;
                 }
-                .extra{
+                .extra {
                     line-height: 10px;
-                    .count{
+                    .count {
                         margin-right: 12px;
                     }
                 }
-                .price{
+                .price {
                     font-weight: 700;
                     line-height: 24px;
-                    .now{
+                    .now {
                         margin-right: 8px;
                         font-size: 14px;
-                        color: rgb(240,20,20);
+                        color: rgb(240, 20, 20);
                     }
-                    .old{
+                    .old {
                         text-decoration: line-through;
                         font-size: 10px;
                         color: rgb(147, 153, 159);
@@ -163,6 +182,7 @@
 }
 </style>
 <script>
+import BScroll from 'better-scroll';
 
 const ERR_OK = 0;
 export default {
@@ -174,7 +194,26 @@ export default {
     data() {
         return {
             goods: [],
+            listHeight: [],
+            scrollY: 0,
         };
+    },
+    computed: {
+        currentIndex() {
+            let height1 = 0;
+            let height2 = 0;
+
+            for (let i = 0; i < this.listHeight.length; i += 1) {
+                height1 = this.listHeight[i];
+                height2 = this.listHeight[i + 1];
+
+                if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+                    return i;
+                }
+            }
+            return 0;
+        },
+
     },
     created() {
         this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
@@ -183,8 +222,37 @@ export default {
             const result = response.body;
             if (ERR_OK === 0) {
                 this.goods = result.data;
+                this.$nextTick(() => {
+                    this.initScroll();
+                    this.calculateHeight();
+                });
             }
         });
+    },
+    methods: {
+        initScroll() {
+            this.menuScroll = new BScroll(this.$refs.menuWrapper, {});
+            this.foodsScroll = new BScroll(this.$refs.foodsWrapper, { probeType: 3 });
+
+            this.foodsScroll.on('scroll', (pos) => {
+                this.scrollY = Math.abs(Math.round(pos.y));
+            });
+        },
+        calculateHeight() {
+            const foodList = this.toArray(this.$refs.foodsWrapper.getElementsByClassName('food-list-hook'));
+            let height = 0;
+            let item = null;
+
+            this.listHeight.push(height);
+            for (item of foodList) {
+                height += item.clientHeight;
+                this.listHeight.push(height);
+            }
+        },
+        toArray(obj) {
+            return Array.prototype.slice.call(obj);
+        },
+
     },
 };
 
